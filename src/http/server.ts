22bd6f877e29dd5332831e52e5630e9df75df1b0
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-02-07 00:33:34 -0300

    feat: feature from create e new Poll created

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,26 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
 
 const app = fastify()
 const PORT = 3333
+
+const prismaClientInstance = new PrismaClient()
+
+app.post('/polls', async (request, reply) => {
+    const createPollBody = z.object({
+        title: z.string()
+    })
+
+    const { title } = createPollBody.parse(request.body)
+    const poll = await prismaClientInstance.poll.create({
+        data: {
+            title 
+        }
+    })
+
+    return reply.status(201).send(poll)
+})
 
 app.listen({ port: PORT }).then(() => {
     console.log('Server running on port: ' + PORT)
