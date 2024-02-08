f438a149ed553db3a35011217807d5e6d3c02b13
diogenesOliver <diogenes.riboliveira@gmail.com>
2024-02-08 17:54:21 -0300

    feat: Registring websocket middleware and a new websocket request

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,11 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
+
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
-import cookie from '@fastify/cookie'
+import { pollResuts } from './ws/poll-results'
 
 const app = fastify()
 const PORT = 3333
@@ -12,9 +15,15 @@ app.register(cookie, {
     hook: 'onRequest',
 })
 
+app.register(websocket)
+
+//HTTP - Request
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+
+//WebSocket - Request
+app.register(pollResuts)
 
 app.listen({ port: PORT }).then(() => {
     console.log('Server running on port: ' + PORT)
